c4b11a1813eb2d4460cdacc7c7674b187f9736eb
Alex Chubaty <achubaty@for-cast.ca>
2022-01-04 12:11:58 -0700

    [WIP] cleanup pkgs and paths

--- 00-global.R
+++ 00-global.R
@@ -16,19 +16,30 @@ if (!suppressWarnings(require("Require"))) {
 pkgs <- c(
   "data.table", "DHARMa", "effects", "foreign", "gamlss", "ggplot2", "lme4", "lmerTest", "mgcv",
   "dplyr", "readr", "tidyverse", ## TODO: remove these in favour of data.table
-  "rgdal", "sf", "terra", "raster", ## TODO: remove raster
   "performance", "splines"
 )
-
 Require(pkgs)
+
+sptlPkgs <- c("rgdal", "sf", "terra", "raster") ## TODO: remove raster
+if (!all(sptlPkgs %in% rownames(installed.packages()))) {
+  install.packages(sptlPkgs, repos = "https://cran.rstudio.com")
+
+  sf::sf_extSoftVersion() ## want GEOS 3.9.0, GDAL 3.2.1, PROJ 7.2.1
+}
+Require(sptlPkgs)
+
+Require("PredictiveEcology/reproducible@development")
+
+cacheDir <- checkPath("cache", create = TRUE)
+inputDir <- checkPath("inputs", create = TRUE)
+outputDir <- checkPath("outputs", create = TRUE)
 
 ## TODO: Alex resume here; data available on Google Drive:
 ## https://drive.google.com/drive/folders/1ZM8i8VZ8BcsxEdxWPE2S-AMO0JvQ9DRI?usp=sharing
 
+
 # run the model
-setwd("C:/Users/Raquel/Dropbox/Working/FIRES_ONTARIO/Goverment/Data/clip")
-getwd()
-plot3 <- read.table("FinalAllAgeDataset2.txt", header = TRUE, sep = " ", fill = TRUE, dec = ".") # the dataset of ground plots
+plot3 <- read.table(file.path(inputDir, "FinalAllAgeDataset2.txt"), header = TRUE, sep = " ", fill = TRUE, dec = ".") # the dataset of ground plots
 plot3$ecozone <- as.factor(as.character(plot3$ecozone))
 summary(plot3$ecozone)
 plot3$LCC <- as.factor(as.character(plot3$LCC))
@@ -41,7 +52,7 @@ summary(modage2)
 gam.check(modage2) #
 
 # I have created the dataset that I will use to predict the model in ArcMap (resolution 1 x 1 km)
-newdataset <- read.table("TaveMultiPoints.txt", header = T, sep = ",", fill = T, dec = ".")
+newdataset <- read.table(file.path(inputDir, "TaveMultiPoints.txt"), header = TRUE, sep = ",", fill = TRUE, dec = ".")
 head(newdataset)
 str(newdataset)
 newdataset <- na.omit(newdataset[, -c(1:2)])
@@ -69,20 +80,19 @@ cor.test(newdataset$PreviusStandAge, newdataset$predictstack)
 hist((newdataset$PreviusStandAge))
 # write.csv(newdataset,"Predictions.csv")# I have exported the dataset and created the raster in ArcMap
 
-predAge <- raster("Predictions_PointToRaster1.tif")
+predAge <- raster(file.path(inputDir, "Predictions_PointToRaster1.tif"))
 plot(predAge)
 
-predPrevAge <- raster("standAgeMap_it_1_ts_2011_ProROF.tif") # This is the previous layer of Stand Age, is very different compared to the new one.
+predPrevAge <- raster(file.path(inputDir, "standAgeMap_it_1_ts_2011_ProROF.tif")) # This is the previous layer of Stand Age, is very different compared to the new one.
 plot(predPrevAge)
 
-# I would like to create the dataset to predict in R.This is what I tried so far.
-setwd("C:/Users/Raquel/Dropbox/Working/FIRES_ONTARIO/Goverment/Data/clip")
-getwd()
 #####
-ba <- raster("CA_forest_basal_area_2015_ROF.tif")
-Tave <- raster("Normal_1981_2010_Tave_sm_ROF.tif")
-ecozone <- raster("ecozones_PolygonToRaster21_C1.tif")
-LCC <- raster("CAN_LC_2015_CAL_Clip1.tif")
+
+## I would like to create the dataset to predict in R. This is what I tried so far.
+ba <- raster(file.path(inputDir, "CA_forest_basal_area_2015_ROF.tif"))
+Tave <- raster(file.path(inputDir, "Normal_1981_2010_Tave_sm_ROF.tif"))
+ecozone <- raster(file.path(inputDir, "ecozones_PolygonToRaster21_C1.tif"))
+LCC <- raster(file.path(inputDir, "CAN_LC_2015_CAL_Clip1.tif"))
 
 ### In ArcGis I have reprojected the rasters, but I am having problems in r (I have removed all my failure code)
 # the idea was to extract the values of the rasters for the layer of points of LCC
